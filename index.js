30a8e6825f46e9c9637b02a7a1f5a7c96a660d77
Harshada Mundwadkar <harshadamundwadkarjnec@gmail.com>
2025-07-05 23:53:11 +0530

    commited

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { uploadVideo } = require("./routes/video");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// ✅ Serve files in /public
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/upload", uploadVideo);
 
 const PORT = process.env.PORT || 5000;
